0eeee33d80f8c176f94b91cbdd9beced7c3d503d
Abhi <abhishekwankhade4670@gmail.com>
2022-08-01 12:54:13 +0530

    add abhi

--- abhi.java
+++ abhi.java
@@ -1,6 +1,11 @@
 public class abhi{
     public static void main(String[] args) {
         System.out.println("hello");
+
+        int a=3;
+        int b= 2;
+        System.out.println(a+b);
     }
     
 }
+ 
